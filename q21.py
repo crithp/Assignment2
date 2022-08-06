5ab056f3e6043547f2bb88f74af3cff9832ad18f
Chris Thornton <christhornton@live.co.za>
2022-08-06 14:12:49 +0200

    Question 2.1 (b)

--- q21.py
+++ q21.py
@@ -1,6 +1,12 @@
 """
     Question 2.1
 """
+from sys import argv
+
+if len(argv) < 1:
+    raise Exception("Please give file as input")
+
+input_file = argv[1]
 
 # Read the file in
 with open('pos_tagged.txt', 'r') as infile:
@@ -31,6 +37,9 @@ for pair in text.split():
 
 print("word", "tag", "probability")
 
+# Stores the most probable tag per word
+most_probable = {}
+
 # Now print out the probabilities of each word-tag pair. Order by occurrences descending for more sense
 for word, obj in sorted(probabilities.items(), key=lambda x: x[1]['occurrences'], reverse=True):
     occurrences = obj['occurrences']
@@ -40,3 +49,23 @@ for word, obj in sorted(probabilities.items(), key=lambda x: x[1]['occurrences']
 
         probability = obj[tag] / occurrences
         print(word, tag, probability)
+
+        # Store the highest probability for the word
+        if word not in most_probable or probability > most_probable[word][1]:
+            most_probable[word] = (tag, probability)
+
+# Now mark every word in the input file
+# Read the file in
+with open(input_file, 'r') as infile:
+    text = infile.read()
+
+with open('output_tagged.txt', 'w+') as outfile:
+    for word in text.split():
+        word = word.strip()
+
+        tag = 'NN'  # Default tag
+
+        if word in most_probable:
+            tag = most_probable[word][0]
+
+        outfile.write(word + "/" + tag + ' ')
